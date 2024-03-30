5fc4e4fa6aea96134b415025bad8236beaf384fd
saisantosh1998 <58393030+saisantosh1998@users.noreply.github.com>
2024-03-30 09:22:00 +0530

    update post model

--- backend/src/models/Post.js
+++ backend/src/models/Post.js
@@ -5,6 +5,10 @@ const postSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  image: {
+    type: String,
+    required: true,
+  },
   content: {
     type: String,
     required: true,
